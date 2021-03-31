cfc193261b3ca987b6b3fe0e2bb4127041605a6e
JohnDanz <John.Danz87@gmail.com>
2021-03-31 14:40:13 -0500

    pulled out some connectors for wallet conenct

--- packages/comps/src/components/common/get-wallet-icon.tsx
+++ packages/comps/src/components/common/get-wallet-icon.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import {fortmatic, injected, portis, walletconnect, walletlink} from '../ConnectAccount/connectors';
+// import {fortmatic, injected, portis, walletconnect, walletlink} from '../ConnectAccount/connectors';
+import { injected, walletconnect } from '../ConnectAccount/connectors';
 import Identicon from '../ConnectAccount/components/Identicon';
 import WalletConnectIcon from '../ConnectAccount/assets/walletConnectIcon.svg';
-import CoinbaseWalletIcon from '../ConnectAccount/assets/coinbaseWalletIcon.svg';
-import FortmaticIcon from '../ConnectAccount/assets/fortmaticIcon.png';
-import PortisIcon from '../ConnectAccount/assets/portisIcon.png';
+// import CoinbaseWalletIcon from '../ConnectAccount/assets/coinbaseWalletIcon.svg';
+// import FortmaticIcon from '../ConnectAccount/assets/fortmaticIcon.png';
+// import PortisIcon from '../ConnectAccount/assets/portisIcon.png';
 import Styles from './get-wallet-icon.styles.less';
-import {TinyButton} from './buttons';
+// import {TinyButton} from './buttons';
 import {AbstractConnector} from '@web3-react/abstract-connector';
-import { useAppStatusStore } from '../../stores/app-status';
+// import { useAppStatusStore } from '../../stores/app-status';
 
 export interface GetWalletIconProps {
   account: string;
@@ -17,7 +18,7 @@ export interface GetWalletIconProps {
 };
 
 export const GetWalletIcon = ({connector, account, showPortisButton = false }: GetWalletIconProps) => {
-  const { isMobile } = useAppStatusStore();
+  // const { isMobile } = useAppStatusStore();
   let icon;
   let iconAlt;
 
@@ -30,18 +31,18 @@ export const GetWalletIcon = ({connector, account, showPortisButton = false }: G
       icon = WalletConnectIcon;
       iconAlt = 'Wallet Connect Logo';
       break;
-    case walletlink:
-      icon = CoinbaseWalletIcon;
-      iconAlt = 'Coinbase Wallet Logo';
-      break;
-    case fortmatic:
-      icon = FortmaticIcon;
-      iconAlt = 'Fortmatic Logo';
-      break;
-    case portis:
-      icon = PortisIcon;
-      iconAlt = 'Portis Logo';
-      break;
+    // case walletlink:
+    //   icon = CoinbaseWalletIcon;
+    //   iconAlt = 'Coinbase Wallet Logo';
+    //   break;
+    // case fortmatic:
+    //   icon = FortmaticIcon;
+    //   iconAlt = 'Fortmatic Logo';
+    //   break;
+    // case portis:
+    //   icon = PortisIcon;
+    //   iconAlt = 'Portis Logo';
+    //   break;
     default:
       return null;
   }
@@ -49,12 +50,12 @@ export const GetWalletIcon = ({connector, account, showPortisButton = false }: G
   return (
     <div className={Styles.WalletIcon}>
       {connector === injected ? icon : <img src={icon} alt={iconAlt} />}
-      {showPortisButton && connector === portis && !isMobile && (
+      {/* {showPortisButton && connector === portis && !isMobile && (
         <TinyButton
           action={() => portis.portis.showPortis()}
           text='Show Portis'
         />
-      )}
+      )} */}
     </div>
   );
 }
