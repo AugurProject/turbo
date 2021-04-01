dff9240a9ca268cd0bf7b00be6ff3149cc1ba3c8
Bruno Confortin <brunoconfortin@gmail.com>
2021-04-01 16:04:46 -0300

    Prettified and linted

--- packages/smart/src/utils/common-functions.ts
+++ packages/smart/src/utils/common-functions.ts
@@ -15,8 +15,8 @@ export type RecursivePartial<T> = {
   [P in keyof T]?: T[P] extends Array<infer U>
     ? Array<RecursivePartial<U>>
     : T[P] extends Record<string, unknown>
-      ? RecursivePartial<T[P]>
-      : T[P];
+    ? RecursivePartial<T[P]>
+    : T[P];
 };
 
 export async function sleep(milliseconds: number): Promise<void> {
