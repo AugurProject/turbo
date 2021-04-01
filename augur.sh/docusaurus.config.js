e3be6a8569be23130b57d389f71346d6830794eb
Paul Gebheim <pgebheim@gmail.com>
2021-04-01 22:39:33 +0000

    Get rid of blog

--- augur.sh/docusaurus.config.js
+++ augur.sh/docusaurus.config.js
@@ -6,15 +6,8 @@ module.exports = {
   onBrokenLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'augurproject', // Usually your GitHub org/user name.
-  projectName: 'augur', // Usually your repo name.
+  projectName: 'turbo', // Usually your repo name.
   stylesheets: [
-    {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css',
-      type: 'text/css',
-      integrity:
-      'sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X',
-      crossorigin: 'anonymous',
-    },
   ],
   themeConfig: {
     navbar: {
@@ -32,7 +25,7 @@ module.exports = {
           position: 'left',
         },
         {
-          href: 'https://github.com/AugurProject/augur',
+          href: 'https://github.com/AugurProject/turbo',
           label: 'GitHub',
           position: 'right',
         },
@@ -49,11 +42,11 @@ module.exports = {
       {
         docs: {
           path: 'docs',
-          // It is recommended to set document id as docs home page (`docs/` path).
+          // Docs-only mode
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/AugurProject/augur/edit/documentation/augur.sh/',
+          editUrl: 'https://github.com/AugurProject/turbo/edit/dev/augur.sh/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
