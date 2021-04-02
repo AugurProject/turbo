08fe9af2693116a94fbbf1d4c5ad620144f15296
phoebemirman <phoebe.mirman@gmail.com>
2021-04-02 10:14:52 -0700

    fix simp build

--- packages/simplified/src/index.tsx
+++ packages/simplified/src/index.tsx
@@ -4,6 +4,7 @@ import App from './modules/App';
 import * as comps from '@augurproject/comps';
 const { windowRef } = comps;
 console.log(comps);
+// @ts-ignore
 windowRef.appStatus = {};
 windowRef.data = {};
 windowRef.user = {};
