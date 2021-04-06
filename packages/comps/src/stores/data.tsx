5c0137d453ccc9c48f15462e5db00fca3d9c22d8
JohnDanz <John.Danz87@gmail.com>
2021-04-06 13:11:51 -0500

    pass markets, instead of marketInfos

--- packages/comps/src/stores/data.tsx
+++ packages/comps/src/stores/data.tsx
@@ -43,12 +43,12 @@ export const DataProvider = ({ children }) => {
       return { marketInfos: {}, ammExchanges: {} };
     };
     getMarkets().then(({ marketInfos, ammExchanges }) => {
-      isMounted && updateDataHeartbeat({ ammExchanges, cashes, marketInfos }, blocknumber ? blocknumber + 1 : 0, null);
+      isMounted && updateDataHeartbeat({ ammExchanges, cashes, markets: marketInfos }, blocknumber ? blocknumber + 1 : 0, null);
     })
     
     const intervalId = setInterval(() => {
       getMarkets().then(({ marketInfos, ammExchanges }) => {
-        isMounted && updateDataHeartbeat({ ammExchanges, cashes, marketInfos }, blocknumber ? blocknumber + 1 : 0, null);
+        isMounted && updateDataHeartbeat({ ammExchanges, cashes, markets: marketInfos }, blocknumber ? blocknumber + 1 : 0, null);
       })
     }, NETWORK_BLOCK_REFRESH_TIME[42]);
     return () => {
