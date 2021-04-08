e8dc9e0b511251340e9824301e6f944aa696e78a
Paul Gebheim <pgebheim@users.noreply.github.com>
2021-04-07 17:07:47 -0700

    Add task to fetch data (#131)
    * Add task to fetch data
    
    * Add missing file
    
    * Add doc for new task

---
+++ packages/smart/tasks/rundown.ts
@@ -0,0 +1,19 @@
+import { task } from "hardhat/config";
+import axios from "axios";
+
+task("fetch-rundown-event", "Retrieve rundown event information")
+  .addParam("event", "The API key for fetching from TheRunDown")
+  .addParam("key", "The API key for fetching from TheRunDown")
+  .setAction(async (args, hre) => {
+    const result = await axios({
+      method: "GET",
+      url: `https://therundown-therundown-v1.p.rapidapi.com/events/${args.event}`,
+      params: { include: "scores" },
+      headers: {
+        "x-rapidapi-key": args.key,
+        "x-rapidapi-host": "therundown-therundown-v1.p.rapidapi.com",
+      },
+    });
+
+    console.log(JSON.stringify(result.data, null, "  "));
+  });
