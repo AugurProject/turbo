319ca28e4c79699e12b5197a74143a6aa83959ff
robert <robert.davidson@forecastfoundation.org>
2021-04-12 10:50:16 -0700

    deploy to kovan

--- packages/smart/addresses.ts
+++ packages/smart/addresses.ts
@@ -26,13 +26,6 @@ export const addresses: AddressMapping = {
     marketFactory: "0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e",
     ammFactory: "0x0DCd1Bf9A1b36cE34237eEaFef220932846BCD82",
   },
-  42: {
-    collateral: "0xc5E8d51C8f5D51061bC43B1831912D4b9760c930",
-    reputationToken: "0x4b9EC1536c315AD87f085a51f69cF14E5680e710",
-    balancerFactory: "0x6B5242382Ad2EbC545FB971B09cD6A95C930023c",
-    marketFactory: "0xAac5DEF20f9Dd1CfcACf98BDDdEE3d15AD65BA8e",
-    ammFactory: "0x4383c9205AE472939cb84005C77be4d71ae2bB60",
-  },
   212984383488152: {
     collateral: "0xe5aa91537A66e884178B2A9faD97afFfe78b5EC5",
     reputationToken: "0xA50667Cf776fcb033001F1ff54fC313E90ABE508",
@@ -46,5 +39,12 @@ export const addresses: AddressMapping = {
     balancerFactory: "0xd62e60a61F033658fc2D16cb82861e5776d1BFDf",
     marketFactory: "0x9a7c7af945bB7684909732a929F3047Dd984380F",
     ammFactory: "0xf0591767C4F1d47c2746387463De4B91A89231c5",
+  },
+  42: {
+    collateral: "0x870C0E536A5f05c87dB2e4A74A4B7321d62b5c3c",
+    reputationToken: "0x556BC70DC25D9DfCdCe584b0D31E9663F7E89391",
+    balancerFactory: "0x71AB67d9ddd30eba963477601017e3c7FC98d819",
+    marketFactory: "0x5277DA83C6F7F7D274aE2e09f4863428fd7274f2",
+    ammFactory: "0x32ae8852Eb6E903685d2cc4a07cB8348e3d4FbAA",
   },
 };
