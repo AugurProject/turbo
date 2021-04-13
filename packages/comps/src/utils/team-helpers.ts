79883c59a9b1ba37e175857b55a9e9400d690e82
JohnDanz <John.Danz87@gmail.com>
2021-04-13 11:26:58 -0500

    added team helpers, added sports.json. generated an updated teams.json to include new sports

---
+++ packages/comps/src/utils/team-helpers.ts
@@ -0,0 +1,24 @@
+import Teams from "./teams.json";
+import Sports from './sports.json';
+
+export interface SportType {
+  sport_id: string;
+  name: string;
+}
+
+export interface TeamType {
+    team_id: number;
+    name: string;
+    mascot: string;
+    abbrevation: string;
+    record?: string;
+    sport_id: string;
+  };
+
+
+export const getTeam = (teamId: string): TeamType => Teams[teamId];
+export const getTeamName = (teamId: string): string => getTeam(teamId)?.name;
+export const getTeamMascot = (teamId: string): string => getTeam(teamId)?.mascot;
+export const getTeamAbbrevation = (teamId: string): string => getTeam(teamId)?.abbrevation;
+export const getSportId = (teamId: string): string => getTeam(teamId)?.sport_id;
+export const getSportName = (teamId: string): SportType => Sports[getSportId(teamId)]?.name;
