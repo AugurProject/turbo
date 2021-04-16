131f81037c266a1fc831ed3c9a36e921525bde43
Tom Haile <bradford.haile@gmail.com>
2021-04-16 10:22:07 -0500

    redeploy so that creating markets script works (#216)
    * redeploy so that creating markets script works
    
    * formatting
    
    * convert type marketId

--- packages/smart/tasks/markets.ts
+++ packages/smart/tasks/markets.ts
@@ -11,9 +11,11 @@ task("markets", "retreive markets").setAction(async (args, hre) => {
   const contracts: ContractInterfaces = buildContractInterfaces(signer, network.chainId);
   const { MarketFactory } = contracts;
 
-  for (let marketId = 0; ; marketId++) {
+  const length = await MarketFactory.marketCount();
+  console.log("length", String(length));
+
+  for (let marketId = 0; marketId < Number(length); marketId++) {
     const market = await MarketFactory.getMarket(marketId);
-    if (market.endTime.eq(0)) break;
     console.log(market);
   }
 });
