f9cec17d8a8c10a5527ab550ea73b70ae9b2dda9
Bruno Confortin <brunoconfortin@gmail.com>
2021-04-26 14:39:08 -0300

    Added graph and AMMFactory entity

--- packages/turbo-kovan-subgraph/src/AMMFactory.ts
+++ packages/turbo-kovan-subgraph/src/AMMFactory.ts
@@ -2,7 +2,9 @@ import { AMMFactory } from "../generated/schema";
 import { PoolCreated } from "../generated/AMMFactory/AMMFactory";
 
 export function handlePoolCreatedEvent(event: PoolCreated): void {
-  let entity = new AMMFactory(event.params.pool.toHex());
+  let id = event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
+  let entity = new AMMFactory(id);
+  entity.pool = event.params.pool;
   entity.marketFactory = event.params.marketFactory;
   entity.marketId = event.params.marketId.toI32();
   entity.creator = event.params.creator;
