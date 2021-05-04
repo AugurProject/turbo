5ede036b3f5a233378cb693cca80fffabfc786da
Bruno Confortin <brunoconfortin@gmail.com>
2021-05-04 03:45:07 -0300

    Checkpoint

--- packages/turbo-kovan-subgraph/src/AbstractMarketFactory.ts
+++ packages/turbo-kovan-subgraph/src/AbstractMarketFactory.ts
@@ -1,5 +1,5 @@
-import { ClaimedProceeds, Outcomes, RemoveLiquidity, Sender, User, WinningsClaimed } from "../generated/schema";
-import { WinningsClaimed as WinningsClaimedEvent } from "../generated/AbstractMarketFactory/AbstractMarketFactory";
+import { ClaimedFees, ClaimedProceeds, Sender } from "../generated/schema";
+import { WinningsClaimed, SettlementFeeClaimed  } from "../generated/AbstractMarketFactory/AbstractMarketFactory";
 
 // WinningsClaimed(
 //   uint256 id,
@@ -21,7 +21,11 @@ import { WinningsClaimed as WinningsClaimedEvent } from "../generated/AbstractMa
 //   tx_hash
 // }
 
-export function handleWinningsClaimedEvent(event: WinningsClaimedEvent): void {
+// collateral == cash
+// shares == amount
+// price is collateral/shares
+
+export function handleWinningsClaimedEvent(event: WinningsClaimed): void {
   const id = event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
   const senderId = event.params.receiver.toHexString();
   const entity = new ClaimedProceeds(id);
@@ -41,6 +45,44 @@ export function handleWinningsClaimedEvent(event: WinningsClaimedEvent): void {
   // entity.cash = event.params.cash;
   // entity.outcome = event.params.outcome;
   // entity.fees = event.params.fees;
+
+  entity.save();
+}
+
+// ClaimedFees {
+//   id
+//   sender {
+//     id
+//   }
+//   cash
+//   marketId
+//   timestamp
+//   tx_hash
+// }
+
+// event SettlementFeeClaimed(
+// address settlementAddress,
+// uint256 amount,
+// address indexed receiver
+// );
+
+export function handleSettlementFeeClaimedEvent(event: SettlementFeeClaimed) {
+  const id = event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
+  const senderId = event.params.settlementAddress.toHexString();
+  const entity = new ClaimedFees(id);
+  let senderEntity = Sender.load(senderId);
+
+  if (senderEntity == null) {
+    senderEntity = new Sender(senderId);
+  }
+
+  senderEntity.save();
+
+  entity.marketId = event.params.marketId.toHexString();
+  entity.transactionHash = event.transaction.hash.toHexString();
+  entity.timestamp = event.block.timestamp;
+  entity.sender = senderId;
+  entity.receiver = event.params.receiver.toHexString();
 
   entity.save();
 }
