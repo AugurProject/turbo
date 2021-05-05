09a342a183aa108f322cd259f7da9c13bee45369
Bruno Goerck Confortin <brunoconfortin@gmail.com>
2021-05-05 15:47:44 -0300

    Bruno/add claims to graph (#402)
    * Checkpoint
    
    * Checkpoint
    
    * Checkpoint
    
    * Checkpoint
    
    * Checkpoint
    
    * Added claim proceeds, fees and prices
    
    * Linted files
    
    * New contract addresses
    
    * Return buy/sell/trade event price as a decimal
    
    * Fix graph mapping warnings
    
    * Fix Trade entity query error
    
    * Linted files
    
    Co-authored-by: justin <328965+justinbarry@users.noreply.github.com>

--- packages/turbo-kovan-subgraph/src/AbstractMarketFactory.ts
+++ packages/turbo-kovan-subgraph/src/AbstractMarketFactory.ts
@@ -1,26 +1,81 @@
-import { User, WinningsClaimed } from "../generated/schema";
-import { WinningsClaimed as WinningsClaimedEvent } from "../generated/AbstractMarketFactory/AbstractMarketFactory";
+import { ClaimedFees, ClaimedProceeds, Sender } from "../generated/schema";
+import { WinningsClaimed, SettlementFeeClaimed } from "../generated/AbstractMarketFactory/AbstractMarketFactory";
+import { bigIntToHexString } from "./utils";
 
-// WinningsClaimed(
-//   uint256 id,
-//   uint256 amount,
-//   address indexed receiver
-// );
-// WinningsClaimed(_id, _winningShares, msg.sender);
+// ClaimedProceeds {
+//   id
+//   sender {
+//     id
+//   }
+//   fees
+//   outcome
+//   marketId
+//   cash
+//   timestamp
+//   tx_hash
+// }
 
-export function handleWinningsClaimedEvent(event: WinningsClaimedEvent): void {
+// collateral == cash
+// shares == amount
+// price is collateral/shares
+
+export function handleWinningsClaimedEvent(event: WinningsClaimed): void {
   const id = event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
-  const entity = new WinningsClaimed(id);
+  const senderId = event.params.receiver.toHexString();
+  const entity = new ClaimedProceeds(id);
+  let senderEntity = Sender.load(senderId);
 
-  let userEntity = User.load(event.params.receiver.toHexString());
-  if (userEntity == null) {
-    userEntity = new User(event.params.receiver.toHexString());
+  if (senderEntity == null) {
+    senderEntity = new Sender(senderId);
   }
-  userEntity.save();
+
+  senderEntity.save();
 
   entity.marketId = event.params.id.toHexString();
-  entity.user = event.params.receiver.toString();
-  entity.winningShares = event.params.amount.toI32();
+  entity.sender = senderId;
+  entity.shares = bigIntToHexString(event.params.amount);
+  entity.outcome = event.params.winningOutcome.toHexString();
+  entity.fees = bigIntToHexString(event.params.settlementFee);
+  entity.transactionHash = event.transaction.hash.toHexString();
+  entity.timestamp = event.block.timestamp;
+
+  entity.save();
+}
+
+// ClaimedFees {
+//   id
+//   sender {
+//     id
+//   }
+//   cash
+//   marketId
+//   timestamp
+//   tx_hash
+// }
+
+// event SettlementFeeClaimed(
+// address settlementAddress,
+// uint256 amount,
+// address indexed receiver
+// );
+
+export function handleSettlementFeeClaimedEvent(event: SettlementFeeClaimed): void {
+  const id = event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
+  const senderId = event.params.settlementAddress.toHexString();
+  const entity = new ClaimedFees(id);
+  let senderEntity = Sender.load(senderId);
+
+  if (senderEntity == null) {
+    senderEntity = new Sender(senderId);
+  }
+
+  senderEntity.save();
+
+  entity.collateral = bigIntToHexString(event.params.amount);
+  entity.sender = senderId;
+  entity.receiver = event.params.receiver.toHexString();
+  entity.transactionHash = event.transaction.hash.toHexString();
+  entity.timestamp = event.block.timestamp;
 
   entity.save();
 }
