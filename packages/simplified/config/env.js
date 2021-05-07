97dc46213c4d4b993ca8471965a10978574b48d3
Paul Gebheim <pgebheim@gmail.com>
2021-05-07 18:56:37 +0000

    Pass-through ETHEREUM_NETWORK into webpack build

--- packages/simplified/config/env.js
+++ packages/simplified/config/env.js
@@ -90,6 +90,8 @@ function getClientEnvironment(publicUrl) {
         // which is why it's disabled by default.
         // It is defined here so it is available in the webpackHotDevClient.
         FAST_REFRESH: process.env.FAST_REFRESH !== 'false',
+        // The network we are connecting to
+        ETHEREUM_NETWORK: process.env.ETHEREUM_NETWORK || 'local'
       }
     );
   // Stringify all values so we can feed into webpack DefinePlugin
