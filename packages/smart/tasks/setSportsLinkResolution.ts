dd672dc50038a0371bfe0af9809e3865493da0a6
Tom Haile <bradford.haile@gmail.com>
2021-05-11 12:03:02 -0500

    Update set resolution task (#476)
    * update to use encoding to set event resolution
    
    * clean up
    
    * create new resolution hardhat task for resolving sports link markets

---
+++ packages/smart/tasks/setSportsLinkResolution.ts
@@ -0,0 +1,27 @@
+import { task, types } from "hardhat/config";
+
+import "hardhat/types/config";
+import { buildContractInterfaces, ContractInterfaces, SportsLinkMarketFactory } from "..";
+import { makeSigner } from "./deploy";
+import { sleep, SportsLinkEventStatus } from "../src";
+
+task("setSportsLinkResolution", "Set market resolution for the SportsLinkMarketFactory")
+  .addParam("marketid", undefined, undefined, types.int)
+  .addParam("eventid", undefined, undefined, types.string)
+  .addParam("homescore", undefined, undefined, types.int)
+  .addParam("awayscore", undefined, undefined, types.int)
+  .setAction(async ({ marketid, eventid, homescore, awayscore }, hre) => {
+    const { ethers } = hre;
+
+    const signer = await makeSigner(hre);
+    const network = await ethers.provider.getNetwork();
+    const contracts: ContractInterfaces = buildContractInterfaces(signer, network.chainId);
+    const { MarketFactories } = contracts;
+    const marketFactory = MarketFactories["sportsball"] as SportsLinkMarketFactory;
+    await marketFactory.trustedResolveMarkets(
+      await marketFactory.encodeResolution(eventid, SportsLinkEventStatus.Final, homescore, awayscore)
+    );
+    await sleep(10000);
+    const market = await marketFactory.getMarket(marketid);
+    console.log(`Set trusted resolution: ${market.winner}`);
+  });
