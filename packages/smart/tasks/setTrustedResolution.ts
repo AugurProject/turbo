736cc975bb298f1e64f7322ac595490821b28429
Tom Haile <bradford.haile@gmail.com>
2021-05-11 10:53:55 -0500

    update to use encoding to set event resolution

--- packages/smart/tasks/setTrustedResolution.ts
+++ packages/smart/tasks/setTrustedResolution.ts
@@ -3,14 +3,16 @@ import { task, types } from "hardhat/config";
 import "hardhat/types/config";
 import { buildContractInterfaces, ContractInterfaces, SportsLinkMarketFactory } from "..";
 import { makeSigner } from "./deploy";
-import { sleep } from "../src";
+import { sleep, SportsLinkEventStatus } from "../src";
 
 task("setTrustedResolution", "Set market resolution for the TrustedMarketFactory")
-  .addParam("market", undefined, undefined, types.int)
-  .addParam("outcomes", undefined, undefined, types.json)
-  .setAction(async ({ turbo: marketId, outcomes }, hre) => {
-    if (!Array.isArray(outcomes) || outcomes.some(isNaN))
-      throw Error(`Outcomes must be an array of strings that represent numbers, not ${outcomes}`);
+  .addParam("marketid", undefined, undefined, types.int)
+  .addParam("eventid", undefined, undefined, types.string)
+  .addParam("homescore", undefined, undefined, types.int)
+  .addParam("awayscore", undefined, undefined, types.int)
+  .setAction(async ({ marketid, eventid, homescore, awayscore }, hre) => {
+    if (!homescore || !awayscore)
+      throw Error(`homeScore and awayScore are required`);
     const { ethers } = hre;
 
     const signer = await makeSigner(hre);
@@ -18,8 +20,10 @@ task("setTrustedResolution", "Set market resolution for the TrustedMarketFactory
     const contracts: ContractInterfaces = buildContractInterfaces(signer, network.chainId);
     const { MarketFactories } = contracts;
     const marketFactory = MarketFactories["sportsball"] as SportsLinkMarketFactory;
-    await marketFactory.trustedSetResolution(marketId, outcomes);
+    await marketFactory.trustedResolveMarkets(
+      await marketFactory.encodeResolution(eventid, SportsLinkEventStatus.Final, homescore, awayscore)
+    );
     await sleep(10000);
-    const market = await marketFactory.getMarket(marketId);
+    const market = await marketFactory.getMarket(marketid);
     console.log(`Set trusted resolution: ${market.winner}`);
   });
