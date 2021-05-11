b1bef57907d0d3b7680e9f86c08d382eeb916d2e
justin <328965+justinbarry@users.noreply.github.com>
2021-05-11 15:07:46 -0700

    Add hardhat task to verify contracts with tenderly
    closes #484

---
+++ packages/smart/tasks/tenderlyVerify.ts
@@ -0,0 +1,28 @@
+import { task } from "hardhat/config";
+import { addresses, ChainId } from "../addresses";
+
+task("tenderly:verify:all", "Push contracts to tenderly", async (args, hre) => {
+  const { chainId } = await hre.ethers.provider.getNetwork();
+
+  const deployedAddresses = addresses[chainId as ChainId];
+  if (!deployedAddresses) return;
+
+  await hre.tenderly.verify([
+    {
+      name: "AMMFactory",
+      address: deployedAddresses.ammFactory,
+    },
+    {
+      name: "BFactory",
+      address: deployedAddresses.balancerFactory,
+    },
+    {
+      name: "Reputation",
+      address: deployedAddresses.reputationToken,
+    },
+    {
+      name: "TheRundownChainlink",
+      address: deployedAddresses.theRundownChainlink,
+    },
+  ]);
+});
