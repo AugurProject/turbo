b7185081cd49a8c4d1e1c4968ab39ad68adb9eed
Tom Haile <bradford.haile@gmail.com>
2021-05-14 17:10:32 -0500

    use pool weights for average price for shares given to user from add/remove li… (#516)
    * use .33 for average price for shares given to user from add/remove liquidity
    
    * formatting
    
    * use pool weights for average price when user gets shares from add/remove liquidity

--- packages/comps/src/utils/contract-calls.ts
+++ packages/comps/src/utils/contract-calls.ts
@@ -985,6 +985,11 @@ const getUserTransactions = (transactions: AllMarketsTransactions, account: stri
   }, {});
 };
 
+const getDefaultPrice = (outcome: string, weights: string[]) => {
+  const total = weights.reduce((p, w) => p.plus(new BN(w)), ZERO);
+  const weight = new BN(weights[Number(outcome)]);
+  return weight.div(total);
+};
 const getInitPositionValues = (
   marketTransactions: MarketTransactions,
   amm: AmmExchange,
@@ -997,32 +1002,38 @@ const getInitPositionValues = (
   const claimTimestamp = lastClaimTimestamp(userClaims?.claimedProceeds, outcomeId, account);
   const sharesEntered = accumSharesPrice(marketTransactions?.buys, outcomeId, account, claimTimestamp);
   const enterAvgPriceBN = sharesEntered.avgPrice;
+  const defaultAvgPrice = getDefaultPrice(outcome, amm.weights);
 
   // get shares from LP activity
-  const sharesAddLiquidity = accumLpSharesPrice(marketTransactions?.addLiquidity, outcomeId, account, claimTimestamp);
+  const sharesAddLiquidity = accumLpSharesPrice(
+    marketTransactions?.addLiquidity,
+    outcomeId,
+    account,
+    claimTimestamp,
+    amm.shareFactor,
+    defaultAvgPrice
+  );
   const sharesRemoveLiquidity = accumLpSharesPrice(
     marketTransactions?.removeLiquidity,
     outcome,
     account,
-    claimTimestamp
+    claimTimestamp,
+    amm.shareFactor,
+    defaultAvgPrice
   );
 
   const positionFromAddLiquidity = sharesAddLiquidity.shares.gt(ZERO);
   const positionFromRemoveLiquidity = sharesRemoveLiquidity.shares.gt(ZERO);
-  const totalLiquidityShares = sharesRemoveLiquidity.totalShares.plus(sharesAddLiquidity.totalShares);
-  const outcomeLiquidityShares = sharesRemoveLiquidity.shares.plus(sharesAddLiquidity.shares);
-  const portion = outcomeLiquidityShares.div(totalLiquidityShares);
-  let netLiquidityCashAmounts = sharesAddLiquidity.cashAmount.minus(sharesRemoveLiquidity.cashAmount).times(portion);
 
-  // TODO: need to know how much collateral didn't get added to pool
-  // in order to get cash amount that went into shares
-  if (sharesRemoveLiquidity.cashAmount.eq(ZERO)) {
-    netLiquidityCashAmounts = ZERO;
-  }
-  // normalize shares amount by div by share factor
+  const outcomeLiquidityShares = sharesRemoveLiquidity.shares.plus(sharesAddLiquidity.shares);
+
   const avgPriceLiquidity = outcomeLiquidityShares.gt(0)
-    ? netLiquidityCashAmounts.div(outcomeLiquidityShares.div(new BN(amm.shareFactor)))
+    ? sharesAddLiquidity.avgPrice
+        .times(sharesAddLiquidity.shares)
+        .plus(sharesRemoveLiquidity.avgPrice.times(sharesRemoveLiquidity.shares))
+        .div(sharesAddLiquidity.shares.plus(sharesRemoveLiquidity.shares))
     : ZERO;
+
   const totalShares = outcomeLiquidityShares.plus(sharesEntered.shares);
   const weightedAvgPrice = totalShares.gt(ZERO)
     ? avgPriceLiquidity
@@ -1063,7 +1074,7 @@ const accumSharesPrice = (
       },
       { shares: ZERO, cashAmount: ZERO, accumAvgPrice: ZERO }
     );
-  const avgPrice = result.accumAvgPrice.div(result.cashAmount);
+  const avgPrice = result.cashAmount.eq(ZERO) ? ZERO : result.accumAvgPrice.div(result.cashAmount);
   return { shares: result.shares, cashAmount: result.cashAmount, avgPrice };
 };
 
@@ -1071,30 +1082,33 @@ const accumLpSharesPrice = (
   transactions: AddRemoveLiquidity[],
   outcome: string,
   account: string,
-  cutOffTimestamp: number
-): { shares: BigNumber; cashAmount: BigNumber; totalShares: BigNumber } => {
-  if (!transactions || transactions.length === 0) return { shares: ZERO, cashAmount: ZERO, totalShares: ZERO };
+  cutOffTimestamp: number,
+  shareFactor: string,
+  outcomeDefaultAvgPrice: string
+): { shares: BigNumber; cashAmount: BigNumber; avgPrice: BigNumber } => {
+  if (!transactions || transactions.length === 0) return { shares: ZERO, cashAmount: ZERO, avgPrice: ZERO };
   const result = transactions
     .filter((t) => isSameAddress(t?.sender?.id, account) && Number(t.timestamp) > cutOffTimestamp)
     .reduce(
       (p, t) => {
-        // todo: need to figure out price for removing liuidity, prob different than add liquidity
-        let shares = t.sharesReturned && t.sharesReturned.length > 0 ? new BN(t.sharesReturned[Number(outcome)]) : ZERO;
+        const outcomeShares = new BN(t.sharesReturned[Number(outcome)]);
+        let shares = t.sharesReturned && t.sharesReturned.length > 0 ? outcomeShares : ZERO;
         if (shares.gt(ZERO) && shares.lte(DUST_POSITION_AMOUNT_ON_CHAIN)) {
-          shares = ZERO;
+          return p;
         }
-        const cashValue = new BN(t.collateral).abs();
-        const total = t.sharesReturned.reduce((p, s) => p.plus(new BN(String(s))), ZERO);
+
+        const cashValue = outcomeShares.eq(ZERO)
+          ? ZERO
+          : outcomeShares.div(new BN(shareFactor)).div(new BN(t.sharesReturned.length)).abs();
         return {
           shares: p.shares.plus(shares),
           cashAmount: p.cashAmount.plus(new BN(cashValue)),
-          totalShares: p.totalShares.plus(total),
         };
       },
-      { shares: ZERO, cashAmount: ZERO, totalShares: ZERO }
+      { shares: ZERO, cashAmount: ZERO }
     );
 
-  return result;
+  return { shares: result.shares, cashAmount: result.cashAmount, avgPrice: new BN(outcomeDefaultAvgPrice) };
 };
 
 export const calculateAmmTotalVolApy = (
