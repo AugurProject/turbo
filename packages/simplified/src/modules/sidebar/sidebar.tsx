6e582432f36d95df8b229aaae82829da340265fe
JohnDanz <John.Danz87@gmail.com>
2021-05-14 13:42:22 -0500

    updated behavior, updated styling for responsive views, re-spaced some of the states with search bar open

--- packages/simplified/src/modules/sidebar/sidebar.tsx
+++ packages/simplified/src/modules/sidebar/sidebar.tsx
@@ -65,7 +65,7 @@ const FilterSideBar = () => {
             setLocalSettings({ ...localSettings, primaryCategory: value, subCategories: [] });
           }}
           title="categories"
-          selected={localSettings.categories}
+          selected={localSettings.primaryCategory}
           items={categoryItems}
         />
         <RadioBarGroup
@@ -105,7 +105,8 @@ const FilterSideBar = () => {
           text="apply filters"
           action={() => {
             updateMarketsViewSettings({
-              categories: localSettings.categories,
+              primaryCategory: localSettings.primaryCategory,
+              subCategories: localSettings.subCategories,
               sortBy: localSettings.sortBy,
               reportingState: localSettings.reportingState,
               currency: localSettings.currency,
