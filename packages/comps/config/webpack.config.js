9bdf64b9f6a79d20e42b1e4faa3fcdf6756a0edf
JohnDanz <John.Danz87@gmail.com>
2021-05-14 20:14:01 -0500

    updated simp and comps to strip console.log/info/warning/debug out for production

--- packages/comps/config/webpack.config.js
+++ packages/comps/config/webpack.config.js
@@ -245,6 +245,12 @@ module.exports = function (webpackEnv) {
               // Pending further investigation:
               // https://github.com/terser-js/terser/issues/120
               inline: 2,
+              pure_funcs: [
+                'console.log', 
+                'console.info', 
+                'console.debug', 
+                'console.warn'
+              ],
             },
             mangle: {
               safari10: true,
