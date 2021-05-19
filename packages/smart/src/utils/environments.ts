8ad6bcb6f709b1cb781956bb348e2247a9fb0dd7
Bruno Confortin <brunoconfortin@gmail.com>
2021-05-19 02:06:22 -0300

    Added environments and mustache template automation

---
+++ packages/smart/src/utils/environments.ts
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const {graphChainNames, addresses} = require("../../addresses");
+
+function generateJsonEnvironments() {
+  const keys = Object.keys(addresses);
+  fs.mkdirSync("environments", {recursive: true});
+  for (let i = 0; i < keys.length; i++) {
+    const file = JSON.stringify(addresses[keys[i]]);
+    fs.writeFileSync(`environments/${graphChainNames[keys[i]]}.json`, file);
+  }
+}
+
+module.exports.generateJsonEnvironments = generateJsonEnvironments;
