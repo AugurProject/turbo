1a423ed4da37cc22bae74bad6e7d107325bebe05
Bruno Confortin <brunoconfortin@gmail.com>
2021-05-19 02:38:34 -0300

    Resolved ts issues

--- packages/smart/src/utils/environments.ts
+++ packages/smart/src/utils/environments.ts
@@ -1,11 +1,13 @@
-const fs = require("fs");
-const { graphChainNames, addresses } = require("../../addresses");
+import fs from "fs";
+import { graphChainNames, addresses } from "../../addresses";
 
 function generateJsonEnvironments() {
   const keys = Object.keys(addresses);
   fs.mkdirSync("environments", { recursive: true });
   for (let i = 0; i < keys.length; i++) {
+    // @ts-ignore
     const file = JSON.stringify(addresses[keys[i]]);
+    // @ts-ignore
     fs.writeFileSync(`environments/${graphChainNames[keys[i]]}.json`, file);
   }
 }
