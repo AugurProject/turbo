f511d864ac2ab29dfb6d648f0249c2579dd70f74
Bruno Confortin <brunoconfortin@gmail.com>
2021-05-19 02:29:46 -0300

    Prettified

--- packages/smart/src/utils/environments.ts
+++ packages/smart/src/utils/environments.ts
@@ -1,9 +1,9 @@
 const fs = require("fs");
-const {graphChainNames, addresses} = require("../../addresses");
+const { graphChainNames, addresses } = require("../../addresses");
 
 function generateJsonEnvironments() {
   const keys = Object.keys(addresses);
-  fs.mkdirSync("environments", {recursive: true});
+  fs.mkdirSync("environments", { recursive: true });
   for (let i = 0; i < keys.length; i++) {
     const file = JSON.stringify(addresses[keys[i]]);
     fs.writeFileSync(`environments/${graphChainNames[keys[i]]}.json`, file);
