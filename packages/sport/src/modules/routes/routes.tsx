a58fade5afae92ddbca1591bf2adb97cead20845
JohnDanz <John.Danz87@gmail.com>
2021-05-20 09:54:51 -0500

    tossed in simp market list, started modifications to get it closer to sports shape

--- packages/sport/src/modules/routes/routes.tsx
+++ packages/sport/src/modules/routes/routes.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
 
 import { Utils } from '@augurproject/comps';
-// import MarketsView from '../markets/markets-view';
+import MarketsView from '../markets/markets-view';
 // import MarketView from '../market/market-view';
 import {
   MARKETS,
@@ -12,14 +12,13 @@ import {
 // import PortfolioView from '../portfolio/portfolio-view';
 const { PathUtils: { makePath } } = Utils;
 
-const fakemarketsView = () => (<div>Markets List</div>);
 const fakemarketView = () => (<div>Market Page</div>);
 const fakemyBets = () => (<div>My Bets</div>);
 const Routes = p => {
   return (
     <Switch>
       <Route path={makePath(PORTFOLIO)} component={fakemyBets} />
-      <Route path={makePath(MARKETS)} component={fakemarketsView} />
+      <Route path={makePath(MARKETS)} component={MarketsView} />
       <Route path={makePath(MARKET)} component={fakemarketView} />
       <Redirect to={makePath(MARKETS)} />
     </Switch>
