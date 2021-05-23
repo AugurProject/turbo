826fd59141bcf075bfcc32facc72f21773eddeca
Robert E. Davidson III <45945043+robert-e-davidson3@users.noreply.github.com>
2021-05-23 13:24:51 -0700

    deploy (#597)
    Co-authored-by: robert <robert.davidson@forecastfoundation.org>

--- packages/smart/addresses.ts
+++ packages/smart/addresses.ts
@@ -41,4 +41,18 @@ export const graphChainNames: {
 type AddressMapping = {
   [id in ChainId]?: Addresses;
 };
-export const addresses: AddressMapping = {};
+export const addresses: AddressMapping = {
+  80001: {
+    reputationToken: "0xD4fB02358d9694539eeD446E6531b1fFbA9b6931",
+    balancerFactory: "0xE152327f9700F1733d12e7a507045FB4A4606C6F",
+    ammFactory: "0x8860542771F787dD8B2c8f9134662751DE2F664f",
+    marketFactories: {
+      sportsball: {
+        type: "SportsLink",
+        address: "0xBC8C695dd045FBfe81C353Fd88E3bedE45C2855D",
+        collateral: "0x5799bFe361BEea69f808328FF4884DF92f1f66f0",
+      },
+    },
+    info: { uploadBlockNumber: 13994149, graphName: "mumbai" },
+  },
+};
