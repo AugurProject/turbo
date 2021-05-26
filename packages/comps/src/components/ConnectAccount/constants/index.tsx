c4ee03edb7b53b72124a101cfd8a1b6f456eae7c
Tom Haile <bradford.haile@gmail.com>
2021-05-26 11:17:56 -0500

    add in the top rpc endpoint lower in the list

--- packages/comps/src/components/ConnectAccount/constants/index.tsx
+++ packages/comps/src/components/ConnectAccount/constants/index.tsx
@@ -16,7 +16,7 @@ export interface WalletInfo {
 
 export const MATIC_MUMBAI_RPCS = ['https://rpc-mumbai.maticvigil.com/', 'https://matic-mumbai.chainstacklabs.com', 'https://matic-testnet-archive-rpc.bwarelabs.com'];
 export const MATIC_MUMBAI_BLOCK_EXPLORERS = ['https://explorer-mumbai.maticvigil.com', 'https://mumbai-explorer.matic.today', 'https://backup-mumbai-explorer.matic.today'];
-export const MATIC_RPCS = ['https://matic-mainnet-full-rpc.bwarelabs.com', 'https://rpc-mainnet.maticvigil.com','https://rpc-mainnet.matic.quiknode.pro','https://matic-mainnet.chainstacklabs.com','https://matic-mainnet-archive-rpc.bwarelabs.com'];
+export const MATIC_RPCS = ['https://matic-mainnet-full-rpc.bwarelabs.com', 'https://rpc-mainnet.maticvigil.com', 'https://matic-mainnet.chainstacklabs.com','https://rpc-mainnet.matic.quiknode.pro', 'https://rpc-mainnet.matic.network/','https://matic-mainnet-archive-rpc.bwarelabs.com'];
 export const MATIC_BLOCK_EXPLORERS = ['https://explorer-mainnet.maticvigil.com','https://explorer.matic.network','https://backup-explorer.matic.network'];
 
 export const MATIC_MUMBAI_RPC_DATA = {
