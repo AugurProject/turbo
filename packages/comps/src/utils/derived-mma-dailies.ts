08f209a4579dab2772562bef8a7a6f682663a331
Tom Haile <bradford.haile@gmail.com>
2021-05-27 15:23:48 -0500

    start focusing on the rules for mma over/under market and head to head, first cut at rules

--- packages/comps/src/utils/derived-mma-dailies.ts
+++ packages/comps/src/utils/derived-mma-dailies.ts
@@ -12,7 +12,7 @@ const NAMING_LINE = {
   OVER_UNDER_LINE: "OVER_UNDER_LINE",
 };
 const NO_CONTEST = "No Contest";
-const NO_CONTEST_TIE = "Tie/No Contest";
+const NO_CONTEST_TIE = "Draw/No Contest";
 
 export const getOutcomeName = (
   outcomeId: number,
@@ -129,27 +129,25 @@ const sportsData = {
   },
 };
 
-// TODO: update rules for MMA
+// TODO: Rules might change depending on how contract resolves over/under MMA markets, need to verify after contract is written
 const sportsResolutionRules = {
   "7": {
     types: {
       [MMA_MARKET_TYPE.MONEY_LINE]: [
-        `At least 55 minutes of play must have elapsed for the game to be deemed official. If the game is not played or if less than 55 minutes of play have been completed, the game is not considered
-an official game and the market should resolve as 'No Contest'.`,
-        `Overtime counts towards settlement purposes.`,
-        `If the game ends in a tie, the market should resolve as 'No Contest'`,
-        `If the game is not played, the market should resolve as 'No Contest'.`,
-        `Results are determined by their natural conclusion and do not recognize postponed games,
-protests, or overturned decisions.`,
+        `A fight is considered official once the first round begins, regardless of the scheduled or actual duration.`,
+        `Market resolves based on the official result immediately following the fight. Later announcements, enquirers, or changes to the official result will not affect market settlement.`,
+        `If a fighter is substituted before the fight begins the market should resolve as 'Draw/No Contest'.`,
+        `If a fighter is disqualified during the fight, the opposing fighter should be declared the winner. If both fighters are disqualified the market should resolve as 'Draw/No Contest'.`,
+        `If the fight is cancelled before it starts for any reason, the market should resolve as 'No Contest'.`,
+        `A draw can occur when the fight is either stopped before completion or after all rounds are completed and goes to the judges' scorecards for decision.  If the match ends in a draw, only the 'Draw/No Contest' result should be the winning outcome.`,
       ],
       [MMA_MARKET_TYPE.OVER_UNDER]: [
-        `At least 55 minutes of play must have elapsed for the game to be deemed official. If the game is
-not played or if less than 55 minutes of play have been completed, the game is not considered
-an official game and the market should resolve as 'No Contest'.`,
-        `Overtime count towards settlement purposes.`,
-        `If the game is not played, the market should resolve as 'No Contest'.`,
-        `Results are determined by their natural conclusion and do not recognize postponed games,
-protests, or overturned decisions.`,
+        `A fight is considered official once the first round begins, regardless of the scheduled or actual duration.`,
+        `Market resolves based on the official result immediately following the fight. Later announcements, enquirers, or changes to the official result will not affect market settlement.`,
+        `If a fighter is substituted before the fight begins the market should resolve as 'Draw/No Contest'.`,
+        `If the fight is cancelled before it starts for any reason, the market should resolve as 'No Contest'.`,
+        `If the official time is exactly on (equal to) the over/under number the market should resolve as 'Over'.`,
+        `Markets referring to round/fight duration represents the actual time passed in the round/fight, as applicable, depending on the scheduled round/fight duration. For example, Over 2.5 Total Rounds will be settled as 'Over' once two and a half minutes or more in the 3rd Round has passed.`,
       ],
     },
   },
