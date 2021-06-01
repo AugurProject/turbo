098cd134c0d0fd1adeb17545918acd5279f422c9
JohnDanz <John.Danz87@gmail.com>
2021-06-01 14:12:10 -0500

    added sports-cards files to sportsbook

---
+++ packages/sport/src/modules/sports-card/sports-card.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Styles from "./sports-card.styles.less";
+
+export const SportsCard = ({ marketId, markets, ammExchanges, timeFormat }) => {
+  return <article className={Styles.SportsMarketCard}></article>;
+};
