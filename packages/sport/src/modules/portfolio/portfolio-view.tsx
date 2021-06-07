32c25caf3518a24796827b37dbe69ae3b07fe58d
JohnDanz <John.Danz87@gmail.com>
2021-06-07 10:22:50 -0500

    added updated mocked out claimable winnings area

--- packages/sport/src/modules/portfolio/portfolio-view.tsx
+++ packages/sport/src/modules/portfolio/portfolio-view.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import Styles from './portfolio-view.styles.less';
-import Activity from './activity';
+import React, { useState } from "react";
+import Styles from "./portfolio-view.styles.less";
+import Activity from "./activity";
 // import { PositionsLiquidityViewSwitcher } from '../common/tables';
 // import { AppViewStats, NetworkMismatchBanner } from '../common/labels';
 import {
@@ -12,34 +12,26 @@ import {
   Stores,
   SEO,
   ButtonComps,
-} from '@augurproject/comps';
-import { PORTFOLIO_HEAD_TAGS } from '../seo-config';
-import { Cash } from '@augurproject/comps/build/types';
+} from "@augurproject/comps";
+import { PORTFOLIO_HEAD_TAGS } from "../seo-config";
+import { Cash } from "@augurproject/comps/build/types";
 
 const { claimWinnings, claimFees } = ContractCalls;
 const { formatCash } = Formatter;
 const { ETH, TX_STATUS, USDC } = Constants;
 const {
-  Hooks: {
-    useDataStore,
-    useAppStatusStore,
-    useScrollToTopOnMount,
-    useUserStore,
-  },
+  Hooks: { useDataStore, useAppStatusStore, useScrollToTopOnMount, useUserStore },
   Utils: { keyedObjToArray },
 } = Stores;
-const { EthIcon, UsdIcon } = Icons;
+const { EthIcon, UsdIcon, WinnerMedal } = Icons;
 const { PrimaryButton } = ButtonComps;
 
 const calculateTotalWinnings = (claimbleMarketsPerCash) => {
-  let total = createBigNumber('0');
+  let total = createBigNumber("0");
   let ids = [];
   let factories = [];
   claimbleMarketsPerCash.forEach(
-    ({
-      ammExchange: { turboId, marketFactoryAddress },
-      claimableWinnings: { claimableBalance },
-    }) => {
+    ({ ammExchange: { turboId, marketFactoryAddress }, claimableWinnings: { claimableBalance } }) => {
       total = total.plus(createBigNumber(claimableBalance));
       // @ts-ignore
       ids.push(turboId);
@@ -57,21 +49,13 @@ const calculateTotalWinnings = (claimbleMarketsPerCash) => {
 export const getClaimAllMessage = (cash: Cash): string => `Claim All ${cash?.name} Winnings`;
 export const getClaimFeesMessage = (cash: Cash): string => `Claim All ${cash?.name} Fees`;
 
-const handleClaimAll = (
-  loginAccount,
-  cash,
-  ids,
-  factories,
-  addTransaction,
-  canClaim,
-  setPendingClaim
-) => {
+const handleClaimAll = (loginAccount, cash, ids, factories, addTransaction, canClaim, setPendingClaim) => {
   const from = loginAccount?.account;
   const chainId = loginAccount?.chainId;
   if (from && canClaim) {
     setPendingClaim(true);
     claimWinnings(from, loginAccount?.library, ids, factories)
-      .then(response => {
+      .then((response) => {
         // handle transaction response here
         setPendingClaim(false);
         if (response) {
@@ -84,32 +68,24 @@ const handleClaimAll = (
             from,
             addedTime: new Date().getTime(),
             message: getClaimAllMessage(cash),
-            marketDescription: '',
+            marketDescription: "",
           });
         }
       })
-      .catch(error => {
+      .catch((error) => {
         setPendingClaim(false);
-        console.log('Error when trying to claim winnings: ', error?.message);
+        console.log("Error when trying to claim winnings: ", error?.message);
       });
-  } 
+  }
 };
 
-const handleClaimFees = (
-  loginAccount,
-  cash,
-  ids,
-  factories,
-  addTransaction,
-  canClaim,
-  setPendingClaimFees
-) => {
+const handleClaimFees = (loginAccount, cash, ids, factories, addTransaction, canClaim, setPendingClaimFees) => {
   const from = loginAccount?.account;
   const chainId = loginAccount?.chainId;
   if (from && canClaim) {
     setPendingClaimFees(true);
     claimFees(from, loginAccount?.library, factories)
-      .then(response => {
+      .then((response) => {
         // handle transaction response here
         setPendingClaimFees(false);
         if (response) {
@@ -122,16 +98,26 @@ const handleClaimFees = (
             from,
             addedTime: new Date().getTime(),
             message: getClaimFeesMessage(cash),
-            marketDescription: '',
+            marketDescription: "",
           });
         }
       })
-      .catch(error => {
+      .catch((error) => {
         setPendingClaimFees(false);
-        console.log('Error when trying to claim winnings: ', error?.message);
+        console.log("Error when trying to claim winnings: ", error?.message);
       });
-  } 
+  }
 };
+
+const ClaimableTicket = ({ amount, button }) => (
+  <section className={Styles.ClaimableTicket}>
+    {WinnerMedal}
+    <p>
+      You have <b>{amount}</b> in winnings to claim in markets
+    </p>
+    {button}
+  </section>
+);
 
 export const ClaimWinningsSection = () => {
   const { isLogged } = useAppStatusStore();
@@ -144,90 +130,99 @@ export const ClaimWinningsSection = () => {
   const [pendingClaim, setPendingClaim] = useState(false);
   const [pendingClaimFees, setPendingClaimFees] = useState(false);
   const { cashes } = useDataStore();
-  const claimableMarkets = marketShares
-    ? keyedObjToArray(marketShares).filter((m) => !!m?.claimableWinnings)
-    : [];
+  const claimableMarkets = marketShares ? keyedObjToArray(marketShares).filter((m) => !!m?.claimableWinnings) : [];
   const keyedCash = keyedObjToArray(cashes);
   const ethCash = keyedCash.find((c) => c?.name === ETH);
   const usdcCash = keyedCash.find((c) => c?.name === USDC);
-  const claimableEthMarkets = claimableMarkets.filter(
-    (m) => m.claimableWinnings.sharetoken === ethCash?.shareToken
-  );
+  const claimableEthMarkets = claimableMarkets.filter((m) => m.claimableWinnings.sharetoken === ethCash?.shareToken);
   const ETHTotals = calculateTotalWinnings(claimableEthMarkets);
   const USDCTotals = calculateTotalWinnings(claimableMarkets);
   // const canClaimETH = useCanExitCashPosition(ethCash);
   const canClaimETH = true;
   const hasClaimableFees = createBigNumber(claimableFees || "0").gt(0);
   const disableClaimUSDCWins =
-  pendingClaim ||
+    pendingClaim ||
     Boolean(transactions.find((t) => t.message === getClaimAllMessage(usdcCash) && t.status === TX_STATUS.PENDING));
   const disableClaimUSDCFees =
-  pendingClaimFees ||
-      Boolean(transactions.find((t) => t.message === getClaimFeesMessage(usdcCash) && t.status === TX_STATUS.PENDING));
+    pendingClaimFees ||
+    Boolean(transactions.find((t) => t.message === getClaimFeesMessage(usdcCash) && t.status === TX_STATUS.PENDING));
+
+  const hasFees = USDCTotals.hasWinnings && ETHTotals.hasWinnings && hasClaimableFees;
 
   return (
     <div className={Styles.ClaimableWinningsSection}>
+      {isLogged && !hasFees && <div>{WinnerMedal} Any winnings will show here</div>}
+      {isLogged && hasFees && (
+        <>
+          <ClaimableTicket amount="$200" button={<PrimaryButton text="CLAIM WINNINGS" action={() => {}} />} />
+          <ClaimableTicket amount="$200" button={<PrimaryButton text="CLAIM WINNINGS" action={() => {}} />} />
+          <ClaimableTicket amount="$200" button={<PrimaryButton text="CLAIM WINNINGS" action={() => {}} />} />
+        </>
+      )}
       {isLogged && USDCTotals.hasWinnings && (
-        <PrimaryButton
-          text={
-            !pendingClaim
-              ? `Claim Winnings (${
-                  formatCash(USDCTotals.total, usdcCash?.name).full
-                })`
-              : `Waiting for Confirmation`
+        <ClaimableTicket
+          amount={formatCash(USDCTotals.total, usdcCash?.name).full}
+          button={
+            <PrimaryButton
+              text={!pendingClaim ? `Claim Winnings` : `Awaiting Signature`}
+              subText={pendingClaim && `(Confirm this transaction in your wallet)`}
+              disabled={disableClaimUSDCWins}
+              action={() => {
+                handleClaimAll(
+                  loginAccount,
+                  usdcCash,
+                  USDCTotals.ids,
+                  USDCTotals.factories,
+                  addTransaction,
+                  true,
+                  setPendingClaim
+                );
+              }}
+            />
           }
-          subText={pendingClaim && `(Confirm this transaction in your wallet)`}
-          disabled={disableClaimUSDCWins}
-          icon={!pendingClaim && UsdIcon}
-          action={() => {
-            handleClaimAll(
-              loginAccount,
-              usdcCash,
-              USDCTotals.ids,
-              USDCTotals.factories,
-              addTransaction,
-              true,
-              setPendingClaim
-            );
-          }}
         />
       )}
       {isLogged && ETHTotals.hasWinnings && (
-        <PrimaryButton
-          text={`${canClaimETH ? '' : 'Approve to '}Claim Winnings (${
-            formatCash(ETHTotals.total, ethCash?.name).full
-          })`}
-          icon={EthIcon}
-          action={() => {
-            handleClaimAll(
-              loginAccount,
-              ethCash,
-              ETHTotals.ids,
-              ETHTotals.factories,
-              addTransaction,
-              canClaimETH,
-              setPendingClaim
-            );
-          }}
+        <ClaimableTicket
+          amount={formatCash(ETHTotals.total, ethCash?.name).full}
+          button={
+            <PrimaryButton
+              text="Claim Winnings"
+              action={() => {
+                handleClaimAll(
+                  loginAccount,
+                  ethCash,
+                  ETHTotals.ids,
+                  ETHTotals.factories,
+                  addTransaction,
+                  canClaimETH,
+                  setPendingClaim
+                );
+              }}
+            />
+          }
         />
       )}
       {isLogged && hasClaimableFees && (
-        <PrimaryButton
-          text={!pendingClaimFees ? `Claim Fees (${
-            formatCash(claimableFees, USDC).full
-          })` : `Waiting for Confirmation`}
-          disabled={disableClaimUSDCFees}
-          action={() => {
-            handleClaimFees(
-              loginAccount,
-              usdcCash,
-              USDCTotals.ids,
-              USDCTotals.factories,
-              addTransaction,
-              true,
-              setPendingClaimFees
-            );
-          }}
+        <ClaimableTicket
+          amount={formatCash(claimableFees, USDC).full}
+          button={
+            <PrimaryButton
+              text={!pendingClaimFees ? `Claim Fees` : `Awaiting Signature`}
+              disabled={disableClaimUSDCFees}
+              action={() => {
+                handleClaimFees(
+                  loginAccount,
+                  usdcCash,
+                  USDCTotals.ids,
+                  USDCTotals.factories,
+                  addTransaction,
+                  true,
+                  setPendingClaimFees
+                );
+              }}
+            />
+          }
         />
       )}
     </div>
@@ -249,19 +244,9 @@ export const PortfolioView = () => {
       <SEO {...PORTFOLIO_HEAD_TAGS} />
       <section>
         <h1>My Bets section... all these components need to be added/worked on but this is the page... yay?</h1>
-        {/* <NetworkMismatchBanner /> */}
-        {/* <AppViewStats small /> */}
-        <ClaimWinningsSection />
-        {/* <PositionsLiquidityViewSwitcher
-          showActivityButton={isMobile}
-          setTables={() => setView(TABLES)}
-          setActivity={() => setView(ACTIVITY)}
-          view={view}
-          claimableFirst
-        /> */}
-        {/* {view === ACTIVITY && <Activity />} */}
       </section>
       <section>
+        <ClaimWinningsSection />
         <Activity />
       </section>
     </div>
