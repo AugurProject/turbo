6fc2957f9b55c2faf1b4db94f010becf9be0a58a
Bruno Confortin <brunoconfortin@gmail.com>
2021-06-10 20:49:26 -0300

    Formatting

--- packages/smart/src/utils/environments.ts
+++ packages/smart/src/utils/environments.ts
@@ -17,8 +17,9 @@ function generateJsonEnvironments() {
     const key = Number(keys[i]) as ChainId;
     const addresses = originalAddresses[key] as EnvironmentAddresses;
     for (let j = 0; j < addresses?.marketFactories.length; j++) {
-      addresses.marketFactories[j].ammFactoryGraphName = j === 0 ? 'AmmFactory' : `AmmFactory${j+1}`;
-      addresses.marketFactories[j].abstractMarketFactoryGraphName = j === 0 ? 'AbstractMarketFactory' : `AbstractMarketFactory${j+1}`;
+      addresses.marketFactories[j].ammFactoryGraphName = j === 0 ? "AmmFactory" : `AmmFactory${j + 1}`;
+      addresses.marketFactories[j].abstractMarketFactoryGraphName =
+        j === 0 ? "AbstractMarketFactory" : `AbstractMarketFactory${j + 1}`;
     }
     // @ts-ignore
     const file = JSON.stringify(addresses);
