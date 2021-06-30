9ff159281500e9e4a03bfe5abcf1f2a1ee3f3bce
JohnDanz <John.Danz87@gmail.com>
2021-06-30 11:42:02 -0500

    finished polishing rules modals

--- packages/sport/src/modules/modal/modal-rules.tsx
+++ packages/sport/src/modules/modal/modal-rules.tsx
@@ -1,6 +1,12 @@
 import React from "react";
-import { Constants, DerivedMarketData } from "@augurproject/comps";
+import Styles from "./modal.styles.less";
+import { Constants, DerivedMarketData, ButtonComps, DateUtils, useAppStatusStore } from "@augurproject/comps";
 import { useMarketEventMarkets } from "../sports-card/sports-card";
+import { Header } from "./common";
+import { useSportsStore } from "../stores/sport";
+
+const { PrimaryThemeButton } = ButtonComps;
+const { getMarketEndtimeFull } = DateUtils;
 const { getResolutionRules } = DerivedMarketData;
 const { SPORTS_MARKET_TYPE_LABELS } = Constants;
 
@@ -18,21 +24,36 @@ const useRules = (marketEvent) => {
 };
 
 export const ModalRules = ({ marketEvent }) => {
+  const {
+    actions: { closeModal },
+  } = useAppStatusStore();
+  const {
+    settings: { timeFormat },
+  } = useSportsStore();
   const shape = useRules(marketEvent);
   return (
-    <section>
-      <h2>{shape.eventDescription}</h2>
-      <h3>{shape.estimatedStart}</h3>
-      {shape.content.map((marketRule) => (
-        <div>
-          <h4>{marketRule.heading}</h4>
-          <ul>
-            {marketRule.rules.map((rule) => (
-              <li>{rule}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
+    <section className={Styles.RulesModal}>
+      <Header title="Event Rules" subtitle={null} />
+      <main>
+        <section>
+          <h2>{shape.eventDescription}</h2>
+          <span>Estimated Start Time:</span>
+          <span>{getMarketEndtimeFull(shape.estimatedStart, timeFormat)}</span>
+        </section>
+        {shape.content.map((marketRule) => (
+          <div>
+            <h4>{marketRule.heading}</h4>
+            <ul>
+              {marketRule.rules.map((rule) => (
+                <li>{rule}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </main>
+      <footer>
+        <PrimaryThemeButton text="Ok" action={closeModal} />
+      </footer>
     </section>
   );
 };
