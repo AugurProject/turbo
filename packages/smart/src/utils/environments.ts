0aec005530f291a47274c6abfca71b565d4ec340
Bruno Confortin <brunoconfortin@gmail.com>
2021-07-09 11:09:09 -0300

    Added automation for specific market factories

--- packages/smart/src/utils/environments.ts
+++ packages/smart/src/utils/environments.ts
@@ -1,30 +1,71 @@
 import fs from "fs";
-import { graphChainNames, addresses as originalAddresses, ChainId, Addresses, MarketFactory } from "../../addresses";
+import { addresses as originalAddresses, Addresses, ChainId, graphChainNames, MarketFactory } from "../../addresses";
 
 interface EnvironmentMarketFactory extends MarketFactory {
-  ammFactoryGraphName: string;
-  abstractMarketFactoryGraphName: string;
+  ammFactoryGraphName?: string;
+  marketFactoryGraphName: string;
 }
 
 interface EnvironmentAddresses extends Addresses {
+  [index: string]: any;
   marketFactories: EnvironmentMarketFactory[];
+  teamSportsMarketFactories: EnvironmentMarketFactory[];
+  mmaMarketFactories: EnvironmentMarketFactory[];
+  cryptoMarketFactories: EnvironmentMarketFactory[];
+}
+
+const marketFactoryTypes: {
+  [index: string]: string | null
+} = {
+  SportsLink: "teamSportsMarketFactories",
+  MMALink: "mmaMarketFactories",
+  Crypto: "cryptoMarketFactories",
+  Trusted: null
+}
+
+const marketFactoryGraphNames: {
+  [index: string]: string | null
+} = {
+  SportsLink: "SportsLinkMarketFactory",
+  MMALink: "MmaMarketFactory",
+  Crypto: "CryptoMarketFactory",
+  Trusted: null
 }
 
 function generateJsonEnvironments() {
-  const keys = Object.keys(originalAddresses);
+  const networks = Object.keys(originalAddresses);
   fs.mkdirSync("environments", { recursive: true });
-  for (let i = 0; i < keys.length; i++) {
-    const key = Number(keys[i]) as ChainId;
+  for (let i = 0; i < networks.length; i++) {
+    const key = Number(networks[i]) as ChainId;
     const addresses = originalAddresses[key] as EnvironmentAddresses;
+    addresses.cryptoMarketFactories = [];
     for (let j = 0; j < addresses?.marketFactories.length; j++) {
       addresses.marketFactories[j].ammFactoryGraphName = j === 0 ? "AmmFactory" : `AmmFactory${j + 1}`;
-      addresses.marketFactories[j].abstractMarketFactoryGraphName =
+      addresses.marketFactories[j].marketFactoryGraphName =
         j === 0 ? "AbstractMarketFactory" : `AbstractMarketFactory${j + 1}`;
+
+      const marketFactoryType = addresses.marketFactories[j].type;
+      const marketFactoryTypeName = marketFactoryTypes.hasOwnProperty(marketFactoryType) ? marketFactoryTypes[marketFactoryType] : null;
+      if (marketFactoryTypeName) {
+        if (!addresses[marketFactoryTypeName]) {
+          addresses[marketFactoryTypeName] = [];
+        }
+
+        const specificFactory = {...addresses.marketFactories[j]};
+        delete specificFactory.ammFactoryGraphName;
+        const marketFactoryGraphName = marketFactoryGraphNames.hasOwnProperty(marketFactoryType) ? marketFactoryGraphNames[marketFactoryType] : null;
+
+        if (marketFactoryGraphName) {
+          specificFactory.marketFactoryGraphName =
+            addresses[marketFactoryTypeName].length === 0 ? marketFactoryGraphName : `${marketFactoryGraphName}${j + 1}`;
+          addresses[marketFactoryTypeName].push(specificFactory);
+        }
+      }
     }
     // @ts-ignore
     const file = JSON.stringify(addresses);
     // @ts-ignore
-    fs.writeFileSync(`environments/${graphChainNames[keys[i]]}.json`, file);
+    fs.writeFileSync(`environments/${graphChainNames[networks[i]]}.json`, file);
   }
 }
 
