b5e9f7b39ad7eb3d328a02fed7a5523266fabe10
JohnDanz <John.Danz87@gmail.com>
2021-07-12 14:11:51 -0500

    removed legacy primary and secondary buttons, legacy tiny still in use for edge cases

--- packages/comps/src/components/common/buttons.tsx
+++ packages/comps/src/components/common/buttons.tsx
@@ -96,17 +96,7 @@ const Button = ({
   );
 };
 
-export const PrimaryButton = (props: ButtonProps) => (
-  <Button
-    {...props}
-    className={classNames(Styles.PrimaryButton, props.className, {
-      [Styles.Dark]: props.darkTheme,
-    })}
-  />
-);
-export const SecondaryButton = (props: ButtonProps) => (
-  <Button {...props} className={classNames(Styles.SecondaryButton, props.className)} />
-);
+
 export const TinyButton = (props: ButtonProps) => (
   <Button {...props} className={classNames(Styles.TinyButton, props.className)} />
 );
