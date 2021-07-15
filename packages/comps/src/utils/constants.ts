653e9ce211ba5357c5d35836401c2213a643585f
JohnDanz <John.Danz87@gmail.com>
2021-07-15 10:49:23 -0500

    added themes to options for sports, added theme useeffect hook to sport store, allow switching between light/dark/auto, dark mode still undefined.

--- packages/comps/src/utils/constants.ts
+++ packages/comps/src/utils/constants.ts
@@ -348,3 +348,14 @@ export const MARKET_LOAD_TYPE = {
   SIMPLIFIED: "SIMPLIFIED",
   SPORT: "SPORT",
 };
+
+export const SPORTS_THEME_TYPES = {
+  SPORT_LIGHT: 'SPORTS',
+  SPORT_DARK: 'SPORTS_DARK',
+};
+  
+// TURBO uses :root styles, so THEME = null
+export const TURBO_THEME_TYPES = {
+  TURBO_LIGHT: null,
+  TURBO_DARK: 'TUBRO_DARK',
+};
