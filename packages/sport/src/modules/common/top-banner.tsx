b775ec3dd37010658b1e15bab723ae907f05f008
John Danz <10524630+JohnDanz@users.noreply.github.com>
2021-07-20 09:27:34 -0500

    Color correction and banner (#979)
    * added more color corrections for dark mode, re-added the top banner for logged out states in sportsbook
    
    * fix empty bets icon on portfolio page

---
+++ packages/sport/src/modules/common/top-banner.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Styles from './top-banner.styles.less';
+
+export const TopBanner = () => (
+  <div className={Styles.TopBanner}>
+    <h1>
+      No Limits. Lower fees.
+      <br />
+      Bet on anything you want.
+    </h1>
+  </div>
+);
