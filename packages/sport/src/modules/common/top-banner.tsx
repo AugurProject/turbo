e62ce0346f080fc1c4566db43b7a9bf0d31969cf
John Danz <10524630+JohnDanz@users.noreply.github.com>
2021-07-30 12:56:40 -0500

    Fix banner (#1014)
    * fixed warning banner collision
    
    * added new banner image file
    
    * updated logged out banner for sportsbook

--- packages/sport/src/modules/common/top-banner.tsx
+++ packages/sport/src/modules/common/top-banner.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
-import Styles from './top-banner.styles.less';
+import React from "react";
+import Styles from "./top-banner.styles.less";
+import { useAppStatusStore, useUserStore, ButtonComps, Constants } from "@augurproject/comps";
 
-export const TopBanner = () => (
-  <div className={Styles.TopBanner}>
-    <h1>
-      No Limits. Lower fees.
-      <br />
-      Bet on anything you want.
-    </h1>
-  </div>
-);
+const { PrimaryThemeButton } = ButtonComps;
+
+export const TopBanner = () => {
+  const {
+    actions: { setModal },
+  } = useAppStatusStore();
+  const { transactions } = useUserStore();
+  return (
+    <div className={Styles.TopBanner}>
+      <h1>
+        Your global no-limit
+        <br />
+        betting platform.
+      </h1>
+      <PrimaryThemeButton
+        text="Connect a wallet to start betting"
+        invert
+        action={() =>
+          setModal({
+            type: Constants.MODAL_CONNECT_WALLET,
+            darkMode: false,
+            autoLogin: false,
+            transactions,
+          })
+        }
+      />
+    </div>
+  );
+};
