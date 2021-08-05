038f4aabf152b243001d8ba311892b1dfe0eaa99
justin <328965+justinbarry@users.noreply.github.com>
2021-08-04 17:12:44 -0700

    Update job creator to exit if bridge already exists

--- packages/chainlink/docker/jobs_creator/run.sh
+++ packages/chainlink/docker/jobs_creator/run.sh
@@ -1,10 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+
+# Wait for node to get online.
+until chainlink bridges list;
+do
+  echo "Trying again";
+done
+
+# Check to see if bridge has already been added.
+chainlink bridges show $BRIDGE_NAME 2> /dev/null;
+if [ $? -eq 0 ]
+then
+  echo "Bridge already created. Exiting....."
+  exit 0;
+fi
 
 echo "Try to create the bridge";
-until chainlink bridges create "{\"name\": \"$BRIDGE_NAME\", \"url\": \"$BRIDGE_URL\",\"confirmations\": 0,\"minimumContractPayment\": \"0\"}";
-do
-  echo "Trying again"
-done
+chainlink bridges create "{\"name\": \"$BRIDGE_NAME\", \"url\": \"$BRIDGE_URL\",\"confirmations\": 0,\"minimumContractPayment\": \"0\"}";
 
 for f in ./templates/*.json; do
   echo "Adding job $f";
