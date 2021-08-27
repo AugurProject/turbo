3a20286b2d9f249a4519649e6158cf3095e2a5c3
JohnDanz <John.Danz87@gmail.com>
2021-08-27 12:58:42 -0500

    base work on mocking out the search filter area + title area

--- packages/simplified/src/modules/liquidity/liquidity-view.tsx
+++ packages/simplified/src/modules/liquidity/liquidity-view.tsx
@@ -1,12 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import Styles from "./liquidity-view.styles.less";
+import { Components } from "@augurproject/comps";
+import { categoryItems } from "../constants";
 import { AppViewStats, AvailableLiquidityRewards } from "../common/labels";
+import { useSimplifiedStore } from "../stores/simplified";
 
-const LiquidityView = () => (
-  <div className={Styles.LiquidityView}>
-    <AppViewStats small liquidity />
-    <AvailableLiquidityRewards />
-  </div>
-);
+const {
+  SelectionComps: { SquareDropdown },
+  InputComps: { SearchInput },
+} = Components;
+
+const LiquidityView = () => {
+  const {
+    marketsViewSettings,
+    actions: { updateMarketsViewSettings },
+  } = useSimplifiedStore();
+  const [filter, setFilter] = useState("");
+  const { primaryCategory } = marketsViewSettings;
+  return (
+    <div className={Styles.LiquidityView}>
+      <AppViewStats small liquidity />
+      <AvailableLiquidityRewards />
+      <h1>Explore LP Opportunties</h1>
+      <p>
+        Add Market liquidity to earn fees and rewards. <a href="#">Learn more →</a>
+      </p>
+      <ul>
+        <SquareDropdown
+          onChange={(value) => {
+            updateMarketsViewSettings({ primaryCategory: value, subCategories: [] });
+          }}
+          options={categoryItems}
+          defaultValue={primaryCategory}
+        />
+        <SquareDropdown
+        onChange={() => {}} options={[
+          {
+            label: 'Daily + Long Term',
+            value: 'daily+long',
+            disabled: false,
+          },
+          {
+            label: 'Daily Only',
+            value: 'daily',
+            disabled: false,
+          },
+          {
+            label: 'Long Term Only',
+            value: 'long',
+            disabled: false,
+          }
+        ]} defaultValue={'daily+long'} />
+        <span>My Liquidity Positions</span>
+        <SearchInput
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+          clearValue={() => setFilter("")}
+          showFilter={true}
+        />
+      </ul>
+    </div>
+  );
+};
 
 export default LiquidityView;
