aef2c7713cf8e13d1226c1eba4252892c69c71f8
Bruno Confortin <brunoconfortin@gmail.com>
2021-09-08 15:59:32 -0300

    Added futures markets v3

--- packages/subgraph/src/helpers/MarketFactoryHelper.ts
+++ packages/subgraph/src/helpers/MarketFactoryHelper.ts
@@ -1,4 +1,12 @@
-import { CryptoMarket, MlbMarket, MmaMarket, NbaMarket, NflMarket, TeamSportsMarket } from "../../generated/schema";
+import {
+  CryptoMarket,
+  FuturesMarket,
+  MlbMarket,
+  MmaMarket,
+  NbaMarket,
+  NflMarket,
+  TeamSportsMarket
+} from "../../generated/schema";
 
 export function getOrCreateTeamSportsMarket(
   id: string,
@@ -120,4 +128,23 @@ export function getOrCreateNbaMarket(id: string, createIfNotFound: boolean = tru
   }
 
   return entity as NbaMarket;
+}
+
+export function getOrCreateFuturesMarket(id: string, createIfNotFound: boolean = true, save: boolean = true): FuturesMarket {
+  let entity = FuturesMarket.load(id);
+
+  if (entity == null && createIfNotFound) {
+    entity = new FuturesMarket(id);
+    let splitId = id.split("-");
+    let MARKET_FACTORY_ID = 0;
+    let MARKET_INDEX = 1;
+    entity.marketFactory = splitId[MARKET_FACTORY_ID];
+    entity.marketIndex = splitId[MARKET_INDEX];
+
+    if (save) {
+      entity.save();
+    }
+  }
+
+  return entity as FuturesMarket;
 }
