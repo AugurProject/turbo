875d8ab7339b4c8ff64f6670dfeb5f7cd55e4362
robert <robert.davidson@forecastfoundation.org>
2021-09-10 10:37:49 -0700

    test where score goes negative

--- packages/smart/test/math.test.ts
+++ packages/smart/test/math.test.ts
@@ -18,6 +18,7 @@ describe("Math", () => {
       { home: 30, away: 37, spread: 7.5, winner: Winner.Home },
       { home: 30, away: 37, spread: 7.0, winner: Winner.Draw },
       { home: 23, away: 20, spread: -3.0, winner: Winner.Draw },
+      { home: 5, away: 1, spread: -9.0, winner: Winner.Away },
     ].forEach(({ home, away, spread, winner }) => {
       it(`calcSpreadWinner(home=${home}, away=${away}, spread=${spread}) => ${winner}`, () => {
         expect(calcSpreadWinner(home, away, spread)).to.equal(winner);
