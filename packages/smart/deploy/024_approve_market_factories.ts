d655115a0e80d8e0449bde311e0361f3aaf74f68
justin <328965+justinbarry@users.noreply.github.com>
2021-09-14 10:56:33 -0700

    Update rewards payout

--- packages/smart/deploy/024_approve_market_factories.ts
+++ packages/smart/deploy/024_approve_market_factories.ts
@@ -11,9 +11,15 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const masterChefDeploy = await deployments.get("MasterChef");
   const masterChef = MasterChef__factory.connect(masterChefDeploy.address, signer);
 
-  const rewardsPerMarket = BigNumber.from(10).pow(18).mul(195);
+  const totalRewardsPerMarket = 195;
+
+  const rewardsPerMarket = BigNumber.from(10)
+    .pow(18)
+    // 80% goes to daily.
+    .mul(totalRewardsPerMarket * 0.8);
   const rewardDaysPerMarket = BigNumber.from(5);
-  const earlyDepositBonusRewards = BigNumber.from(0);
+  // 20% goes to early bonus.
+  const earlyDepositBonusRewards = BigNumber.from(totalRewardsPerMarket * 0.2);
 
   await deployments
     .get("CryptoMarketFactoryV3")
