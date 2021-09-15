a94d51fe69a01f56bfb8c37338a312409011117e
Bruno Confortin <brunoconfortin@gmail.com>
2021-09-15 12:42:57 -0300

    Added sharesMinted to all v3 market factories

--- packages/subgraph/src/mappings/MlbMarketFactoryV3.ts
+++ packages/subgraph/src/mappings/MlbMarketFactoryV3.ts
@@ -6,11 +6,16 @@ import {
   MarketResolved,
   MlbMarketFactory as MlbMarketFactoryContract,
   MlbMarketFactory__getMarketResultValue0Struct,
-  WinningsClaimed
+  WinningsClaimed,
+  SharesMinted
 } from "../../generated/MlbMarketFactoryV3/MlbMarketFactory";
 import { getOrCreateClaimedProceeds } from "../helpers/AbstractMarketFactoryHelper";
 import { bigIntToHexString, SHARES_DECIMALS, USDC_DECIMALS, ZERO } from "../utils";
-import { getOrCreateInitialCostPerMarket, getOrCreatePositionBalance } from "../helpers/CommonHelper";
+import {
+  getOrCreateInitialCostPerMarket,
+  getOrCreatePositionBalance,
+  getOrCreateSharesMinted
+} from "../helpers/CommonHelper";
 
 function getShareTokens(contractAddress: Address, marketId: BigInt): Array<string> {
   let contract = MlbMarketFactoryContract.bind(contractAddress);
@@ -180,4 +185,18 @@ function handlePositionFromClaimWinningsEvent(event: WinningsClaimed): void {
   positionBalanceEntity.save();
 
   closeAllPositions(event.address, event.params.id, marketId, senderId);
+}
+
+export function handleSharesMintedEvent(event: SharesMinted): void {
+  let id = event.transaction.hash.toHexString() + "-" + event.address.toHexString() + "-" + event.params.id.toString() + "-" + event.params.receiver.toHexString();
+  let entity = getOrCreateSharesMinted(id, true, false);
+  entity.transactionHash = event.transaction.hash.toHexString();
+  entity.timestamp = event.block.timestamp;
+  entity.marketFactory = event.address.toHexString();
+  entity.marketIndex = event.params.id.toString();
+  entity.amount = event.params.amount;
+  entity.amountBigDecimal = event.params.amount.toBigDecimal().div(SHARES_DECIMALS);
+  entity.receiver = event.params.receiver.toHexString();
+  entity.receiverId = event.params.receiver.toHexString();
+  entity.save();
 }
