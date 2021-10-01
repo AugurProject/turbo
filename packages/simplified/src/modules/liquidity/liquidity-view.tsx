b726657addd1ca8d72f53b07588703cf52aba297
John Danz <10524630+JohnDanz@users.noreply.github.com>
2021-10-01 14:31:38 -0500

    added liquidity count, renamed liquidity page to pools, updated mobile to allow pools page if not logged in, updated liquidity page to remove initial value as it does not reflect reality. (#1355)

--- packages/simplified/src/modules/liquidity/liquidity-view.tsx
+++ packages/simplified/src/modules/liquidity/liquidity-view.tsx
@@ -224,7 +224,7 @@ const LiquidityMarketCard = ({ market }: LiquidityMarketCardProps): React.FC => 
       <span>{formattedApy || "-"}</span>
       <span>
         {userHasLiquidity ? formatCash(userHasLiquidity?.usdValue, currency).full : "$0.00"}
-        {userHasLiquidity && <span>Init Value {formatCash(userHasLiquidity?.usdValue, currency).full}</span>}
+        {/* {userHasLiquidity && <span>Init Value {formatCash(userHasLiquidity?.usdValue, currency).full}</span>} */}
       </span>
       <span>
         {rewardAmount.formatted} wMATIC
@@ -234,7 +234,7 @@ const LiquidityMarketCard = ({ market }: LiquidityMarketCardProps): React.FC => 
         <div className={Styles.MobileLabel}>
           <span>My Liquidity</span>
           <span>{userHasLiquidity ? formatCash(userHasLiquidity?.usdValue, currency).full : "$0.00"}</span>
-          <span>init. value {formatCash(userHasLiquidity?.initCostUsd, currency).full}</span>
+          {/* <span>init. value {formatCash(userHasLiquidity?.initCostUsd, currency).full}</span> */}
         </div>
         <div className={Styles.MobileLabel}>
           <span>My Rewards</span>
@@ -389,7 +389,7 @@ const LiquidityView = () => {
             clean
             setToggle={() => updatePoolsViewSettings({ onlyUserLiquidity: !onlyUserLiquidity })}
           />
-          My Liquidity Positions
+          {`My Liquidity Positions ${userMarkets.length > 0 ? `(${userMarkets.length})` : ''}`}
         </label>
         <SearchInput value={filter} onChange={(e) => setFilter(e.target.value)} clearValue={() => setFilter("")} />
       </ul>
