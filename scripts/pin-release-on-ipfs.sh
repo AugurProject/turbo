edd538ee34ac4440008f740d4b483e8e62317e6e
Paul Gebheim <pgebheim@users.noreply.github.com>
2021-10-15 09:03:12 -0700

    Add sport release (#1472)
    * Update cd-release.yml
    
    * Update pin-release-on-ipfs.sh
    
    * Update cd-release.yml
    
    * Update pin-release-on-ipfs.sh

--- scripts/pin-release-on-ipfs.sh
+++ scripts/pin-release-on-ipfs.sh
@@ -3,9 +3,10 @@
 # Found: https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
 CURRENT_VERSION=$(curl --silent "https://api.github.com/repos/AugurProject/turbo/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/');
 VERSION="${VERSION:-$CURRENT_VERSION}"
-curl -OL "https://github.com/AugurProject/turbo/releases/download/$VERSION/release.tar.gz";
+APP="${APP:-turbo}
+curl -OL "https://github.com/AugurProject/turbo/releases/download/$VERSION/$APP.tar.gz";
 
-tar -xzf release.tar.gz;
+tar -xzf $APP.tar.gz;
 
 IPFS_HASH_CIDv0=$(ipfs add -rq --silent build | sed '$!d');
 IPFS_HASH_CIDv1=$(ipfs cid base32 $IPFS_HASH_CIDv0);
